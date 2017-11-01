34741d51efd1f83c780f1374f6714fae5af0243a
Rama Kathare <ramakathare@gmail.com>
2017-11-01 09:55:02 +0530

    Initial Commit

--- NocApi.Server/app.js
+++ NocApi.Server/app.js
@@ -1,9 +1,14 @@
-﻿import debug = require('debug');
-import express = require('express');
-import path = require('path');
+﻿'use strict';
+var debug = require('debug');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-import routes from './routes/index';
-import users from './routes/user';
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -11,6 +16,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -19,7 +30,7 @@ app.use('/users', users);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
-    err['status'] = 404;
+    err.status = 404;
     next(err);
 });
 
@@ -28,8 +39,8 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use((err: any, req, res, next) => {
-        res.status(err['status'] || 500);
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
         res.render('error', {
             message: err.message,
             error: err
@@ -39,7 +50,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use((err: any, req, res, next) => {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
